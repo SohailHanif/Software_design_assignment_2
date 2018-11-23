a74ccc742ca9c08c36b294881c972ed66bb3598f
Your Name <you@example.com>
2018-11-23 17:30:10 -0500

    Starting the assignment

---
+++ Assignment1_SoftwareDesign/src/assignment1_softwaredesign/Assignment1_SoftwareDesign.java
@@ -0,0 +1,22 @@
+/*
+-------------------------------------------
+Student: pickco
+StudentID: 911437797
+-------------------------------------------
+ */
+package assignment1_softwaredesign;
+
+/**
+ *
+ * @author Connor Pick pickco@sheridan.desire2learn.com
+ */
+public class Assignment1_SoftwareDesign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
