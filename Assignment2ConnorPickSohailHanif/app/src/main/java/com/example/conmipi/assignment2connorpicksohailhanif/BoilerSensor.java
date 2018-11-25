d56a02c9f24688db9dd0168e8fa80a2d8cf87637
Your Name <you@example.com>
2018-11-24 20:03:42 -0500

    mypart

--- Assignment2ConnorPickSohailHanif/app/src/main/java/com/example/conmipi/assignment2connorpicksohailhanif/BoilerSensor.java
+++ Assignment2ConnorPickSohailHanif/app/src/main/java/com/example/conmipi/assignment2connorpicksohailhanif/BoilerSensor.java
@@ -1,4 +1,30 @@
 package com.example.conmipi.assignment2connorpicksohailhanif;
 
 public class BoilerSensor extends SensorSuperClass {
+
+    boolean waterAdded;
+
+    public BoilerSensor(){
+        super();
+        waterAdded = false;
+    }
+
+    public boolean getBoilerStatus() {
+        if(waterAdded){
+            sensorsOkay = true;
+        }
+        else {
+            sensorsOkay = false;
+        }
+
+        return waterAdded;
+    }
+
+    public void startBoiling(){
+        //Boiler boil = new Boiler();
+    }
+
+    public void addWater(){
+        waterAdded = true;
+    }
 }
--- Assignment2ConnorPickSohailHanif/app/src/main/java/com/example/conmipi/assignment2connorpicksohailhanif/SensorSuperClass.java
+++ Assignment2ConnorPickSohailHanif/app/src/main/java/com/example/conmipi/assignment2connorpicksohailhanif/SensorSuperClass.java
@@ -1,4 +1,12 @@
 package com.example.conmipi.assignment2connorpicksohailhanif;
 
 public class SensorSuperClass {
+
+    boolean sensorsOkay;
+
+    public SensorSuperClass(){
+        sensorsOkay = true;
+    }
+
+
 }
