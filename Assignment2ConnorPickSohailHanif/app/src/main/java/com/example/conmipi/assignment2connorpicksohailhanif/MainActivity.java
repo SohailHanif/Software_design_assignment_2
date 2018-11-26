1fd079a48e4c561703f2c43dba03aaf43c291c4b
SohailHanif <hanifso@sheridanc.on.ca>
2018-11-25 20:07:41 -0500

    Finished sequence diagram, added textviews to android app

--- Assignment2ConnorPickSohailHanif/app/src/main/java/com/example/conmipi/assignment2connorpicksohailhanif/MainActivity.java
+++ Assignment2ConnorPickSohailHanif/app/src/main/java/com/example/conmipi/assignment2connorpicksohailhanif/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button brewButton,addWaterButton,addPotButton;
+    TextView txtBrewStatus, txtWaterStatus, txtPotStatus;
     BoilerSensor b;
     WarmerPlateSensor w;
 
@@ -18,16 +20,24 @@ public class MainActivity extends AppCompatActivity {
         brewButton = (Button) findViewById(R.id.brew_button);
         addWaterButton = (Button) findViewById(R.id.add_water_button);
         addPotButton = (Button) findViewById(R.id.add_pot_button);
+
+        txtBrewStatus = (TextView) findViewById(R.id.txtBrewStatus);
+        txtWaterStatus = (TextView) findViewById(R.id.txtWaterStatus);
+        txtPotStatus = (TextView) findViewById(R.id.txtPotStatus);
+
         b = new BoilerSensor();
         w = new WarmerPlateSensor();
     }
 
-    private void setupButton(){
+    public void onClickBrewBtn(){
         brewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(b.getBoilerStatus() && w.getWarmerPlateStatus()){
-                    b.startBoiling();
+                if(!b.getBoilerStatus()){
+                    txtWaterStatus.setText("Water tank is empty");
+                }
+                else if (!w.getWarmerPlateStatus()){
+                    txtPotStatus.setText("Pot is not on the warmer plate");
                 }
             }
         });
